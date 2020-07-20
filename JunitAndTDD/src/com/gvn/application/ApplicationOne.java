844137e0eef06ff1e77713f3d739c4e1e1b7bf28
venkatnarayan0938 <60142552+venkatnarayan0938@users.noreply.github.com>
2020-07-20 22:01:57 +0530

    Add files via upload

---
+++ JunitAndTDD/src/com/gvn/application/ApplicationOne.java
@@ -0,0 +1,11 @@
+package com.gvn.application;
+
+public class ApplicationOne {
+ 
+		public static void main(String [] args )
+		{
+			System.out.println("Welcome to EEPAM PEP Training");
+			System.out.println("Here you can will leran all concepts in JAVA");
+		}
+	
+}
---
+++ JunitAndTDD/src/com/gvn/application/ApplicationOneTest.java
@@ -0,0 +1,19 @@
+package com.gvn.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationOneTest {
+
+	/**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        assertTrue(true);
+    }
+
+}
